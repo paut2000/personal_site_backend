7caf5fb86f76454a06a0f9061d2cf99b053db286
molner <molner.vasilii@gmail.com>
2022-09-16 11:12:04 +0700

    start

---
+++ src/main/java/ru/molner/personal_site_backend/PersonalSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.molner.personal_site_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalSiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/molner/personal_site_backend/PersonalSiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.molner.personal_site_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
